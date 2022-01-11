1a246918f23b9be65d8d61a5542b3b49e06fd237
leblair <bryan.a.a7@gmail.com>
2022-01-11 18:15:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/drawer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.drawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //token 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
+    }
+}
